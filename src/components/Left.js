64c3799fb2e9f4283f29f9fcb036b56d1eed45d3
srikanthbisai <srikanthbisai2110@gmail.com>
2024-08-31 14:44:09 +0530

    almost done

--- src/components/Left.js
+++ src/components/Left.js
@@ -1,32 +1,97 @@
-import React from 'react'
-
 function Left() {
   return (
-    <div className='flex flex-col sm:hidden md:block md:w-[10%] xl:w-[15%] bg-[#FAFAFA] min-h-screen'>
-      <div className="flex flex-row space-x-2 justify-center mt-5 items-center">
-         <img src='icon.png' alt=''></img>
-         <p className='text-[#0A337A] font-bold text-2xl hidden lg:block'>Vasitum</p>
-      </div>
-      
-      <div className="mainmenu text-sm">
-        <ul className=' flex flex-col pl-10 mt-10 space-y-8'>
-           <li>Main Menu</li>
-           <li className='flex flex-row space-x-6 items-center'><img src='ic_dashboard.png'/> <h1 className='hidden xl:block'>Dashboard</h1></li> 
-           <li className='flex flex-row space-x-6 items-center'><img src='ic_recruitment.png'/> <h1 className='hidden xl:block'>Recruitment</h1></li> 
-           <li className='flex flex-row space-x-6 items-center'><img src='ic_calendar.png'/> <h1 className='hidden xl:block'>Schedule</h1></li> 
-           <li className='flex flex-row space-x-7 items-center'><img src='Group.png'/> <h1 className='hidden xl:block'>Employee</h1></li> 
-           <li className='flex flex-row space-x-6 items-center'><img src='ic_department.png'/> <h1 className='hidden xl:block'>Department</h1></li> 
-        </ul>
+    <div className="flex sm:hidden md:block md:w-[15%] md:m-x-auto xl:w-[15%] bg-[#FAFAFA] min-h-screen">
+      <div>
+        <div className="flex flex-row space-x-3 justify-center mt-10 items-center">
+          <img src="icon.png" alt=""></img>
+          <p className="text-[#0A337A] font-bold text-3xl hidden xl:block">
+            Vasitum
+          </p>
+        </div>
 
+        <div className="mainmenu text-sm ">
+          <ul className=" flex flex-col mt-20 space-y-10 max-md:justify-center max-md:items-center">
+            <li className="font-thin md:text-center lg:text-left lg:ml-10 xl:text-lg">
+              MAIN MENU
+            </li>
 
-        <ul className=' flex flex-col pl-10 space-y-8 mt-16'>
-            <li>Other</li>
-           <li className='flex flex-row space-x-7 items-center'><img src='ic_support.png'/> <h1 className='hidden xl:block'>Settings</h1></li> 
-           <li className='flex flex-row space-x-8 items-center'><img src='Group2.png'/> <h1 className='hidden xl:block'>Schedule</h1></li> 
-        </ul>
+            <div className="flex space-x-10 items-center">
+              <div className="ml-10">
+                <img src="ic_dashboard.png" alt="" />
+              </div>
+
+              <div>
+                <h1 className="hidden xl:block text-lg">Dashboard</h1>
+              </div>
+            </div>
+
+            <div className="flex space-x-10 items-center">
+              <div className="ml-10">
+                <img src="ic_recruitment.png" alt="" />
+              </div>
+
+              <div>
+                <h1 className="hidden xl:block text-lg">Recruitment</h1>
+              </div>
+            </div>
+
+            <div className="flex space-x-10 items-center">
+              <div className="ml-10">
+                <img src="ic_calendar.png" alt="" />
+              </div>
+
+              <div>
+                <h1 className="hidden xl:block text-lg">Schedule</h1>
+              </div>
+            </div>
+
+            <div className="flex space-x-10 items-center">
+              <div className="ml-10">
+                <img src="ic_employee.png" alt="" />
+              </div>
+
+              <div>
+                <h1 className="hidden xl:block text-lg">Employee</h1>
+              </div>
+            </div>
+
+            <div className="flex space-x-10 items-center">
+              <div className="ml-10">
+                <img src="ic_department.png" alt="" />
+              </div>
+
+              <div>
+                <h1 className="hidden xl:block text-lg">Department</h1>
+              </div>
+            </div>
+          </ul>
+
+          <ul className=" flex flex-col mt-20 space-y-10 ">
+            <li className="font-thin xl:text-lg ml-10">OTHER</li>
+            <div className="flex space-x-10 items-center">
+              <div className="ml-10">
+                <img src="ic_support.png" alt="" />
+              </div>
+
+              <div>
+                <h1 className="hidden xl:block text-lg">Support</h1>
+              </div>
+            </div>
+
+            <div className="flex space-x-10 items-center">
+              <div className="ml-10">
+                <img src="settings.png" alt="" />
+              </div>
+
+              <div>
+                <h1 className="hidden xl:block text-lg">Settings</h1>
+              </div>
+            </div>
+          </ul>
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Left
+export default Left;
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,55 +1,64 @@
 function Main() {
   return (
-    <div className="flex  w-[80%] flex-col space-y-5 ml-10">
-      <div className="header pt-6 pb-6 flex justify-between lg:w-[90%]">
-        <div className="flex flex-row">
+    <div className="flex sm:m-auto md:w-full flex-col space-y-5 md:ml-10">
+      <div className="header pt-6 pb-6 flex justify-between items-center sm:space-x-4  lg:pr-20 lg:w-full">
+        <div className="flex flex-row justify-between items-center">
           <img src="menu.png" alt="" className=" hidden sm:block lg:hidden" />
-          <button className="border rounded-lg border-gray-200 hidden md:block md:w-[300px] lg:w-[500px] p-3 flex justify-between">
-            <p>Search</p>
-            <img src="Frame 52.png" alt="" width={10} />
-          </button>
 
-          <img src="search.png" alt="" className="hidden sm:block lg:hidden" />
+          <div className=" flex border rounded-lg hidden md:block md:w-[300px] lg:w-[400px] md:p-1 lg:p-4  items-center justify-between">
+            <p>Search</p>
+            <img src="search.png" alt="" width={30} className="" />
+          </div>
         </div>
 
-        <div className="flex mr-20 space-x-10 items-center justify-center">
+        <div className="flex md:pl-10 mr-20 space-x-6 items-center justify-around">
           <img src="ic_round-notifications.png" alt="" />
           <img src="bi_chat-left-dots-fill.png" alt="" />
           <img src="Mask Group.png" alt="" width={50} />
-          <p className="text-center">hfiwhqw9oj () </p>
+          <p className="text-center font-bold text-[#161E54] text-xl hidden xl:block">
+            Admirra John{" "}
+          </p>
+          <img src="Frame 10.png" alt="" />
         </div>
       </div>
-
-      <div className="container flex sm:flex-col lg:flex-row space-x-3">
-        <div className="xl:w-[60%] flex flex-col">
-          <p>Dashboard</p>
-          <div className="smallcontainer flex flex-row space-x-4 sm:flex-col md:max-w-full md:flex-row">
-            <div className=" md:w-1/3 border border-gray-200 shadow-lg p-3 rounded-md bg-[#FFEFE7] space-y-6">
-              <h1 className="text-2xl text-[#161E54] font-bold">
+      {/* first container */}
+      <div className="containe flex sm:flex-col lg:flex-row">
+        <div className="xl:w-[60%] xl:m-x-auto xl:m-y-auto flex flex-col sm:px-0 md:px-6">
+          <p className="text-4xl text-[#161E54] font-bold font-serif">
+            Dashboard
+          </p>
+          <div className="smallcontainer flex flex-row sm:space-y-5 md:space-y-0 md:space-x-4 sm:flex-col  md:flex-row mt-6 justify-center items-center">
+            <div className="sm:w-full md:w-1/3 border border-gray-200 shadow-lg p-3 rounded-md bg-[#FFEFE7] space-y-6">
+              <h1 className=" sm:text-md xl:text-xl text-[#161E54] font-bold">
                 Available Position
               </h1>
               <h1 className="text-5xl font-semibold">24</h1>
-              <h1 className="text-red-500 text-xl font-sans">
+              <h1 className="text-red-500 md:text-md xl:text-xl font-sans">
                 4 Urgently needed
               </h1>
             </div>
-            <div className="md:w-1/3 border border-gray-200 shadow-lg p-3 rounded-md bg-[#E8F0FB] space-y-6">
-              <h1 className="text-2xl text-[#161E54] font-bold">Job Open</h1>
+
+            <div className="sm:w-full md:w-1/3 border border-gray-200 shadow-lg p-3 rounded-md bg-[#E8F0FB] space-y-6">
+              <h1 className="sm:text-md xl:text-2xl text-[#161E54] font-bold">
+                Job Open
+              </h1>
               <h1 className="text-5xl font-semibold">10</h1>
-              <h1 className="text-xl font-sans text-[#3786F1]">
+              <h1 className="sm:text-md xl:text-xl font-sans text-[#3786F1]">
                 4 Active hiring
               </h1>
             </div>
-            <div className="md:w-1/3 border border-gray-200 shadow-lg p-3 rounded-md bg-[#FDEBF9] space-y-6">
-              <h1 className="text-2xl text-[#161E54] font-bold">
+            <div className="sm:w-full md:w-1/3 border border-gray-200 shadow-lg p-3 rounded-md bg-[#FDEBF9] space-y-6">
+              <h1 className="sm:text-md xl:text-2xl text-[#161E54] font-bold">
                 New Employee
               </h1>
               <h1 className="text-5xl font-semibold">24</h1>
-              <h1 className="text-[#EE61CF] text-xl font-sans">4 Department</h1>
+              <h1 className="text-[#EE61CF] sm:text-md xl:text-xl font-sans">
+                4 Department
+              </h1>
             </div>
           </div>
 
-          <div className="smallcontainer2 flex sm:flex-col max-sm:justify-center max-sm:items-center md:flex-row mt-10 space-x-2">
+          <div className="smallcontainer2 sm:space-y-5 md:space-y-0 md:space-x-3 flex sm:flex-col max-sm:justify-center max-sm:items-center md:flex-row mt-6 ">
             <div className="lg:w-1/2 flex flex-row border border-red-200 rounded-md shadow-lg p-4 justify-between">
               <div className="textContainer text-sm ml-3">
                 <p className="text-2xl font-bold">Total Employees</p>
@@ -74,31 +83,34 @@ function Main() {
             </div>
           </div>
 
-          <div className="lastContainer rounded-lg border border-gray-200 shadow-lg p-0 flex flex-col mt-10 space-y-6">
-            <div className="top flex flex-row justify-between">
-              <h1 className="text-xl font-bold p-3 ml-4">Announcement</h1>
-              <button className="p-3">Click me</button>
+          <div className="rounded-lg border border-gray-200 shadow-lg  flex flex-col mt-6 space-y-6">
+            <div className="top flex flex-row justify-between px-3 pt-3">
+              <h1 className="text-3xl font-bold p-3 ">Announcement</h1>
+              <button className="text-sm border border-gray-200 p-2 flex justify-center items-center rounded-lg">
+                Today, 13 Sept 2021
+                <img src="Frame 10.png" alt="" />
+               </button>
             </div>
 
-            <div className="border border-gray-200 p-2 rounded-lg mt-5 justify-between flex bg-[#FAFAFA]  w-[95%] mx-auto">
+            <div className="border border-gray-200 p-1 rounded-lg mt-5 justify-between flex bg-[#FAFAFA]  w-[95%] mx-auto">
               <div className="textDiv">
                 <h1>Outing Schedule for Every department</h1>
                 <h1 className="text-sm font-thin">5 min ago</h1>
               </div>
-              <div className="imgDiv flex justify-center items-center gap-2">
-                <img src="Group2.png" alt="" />
-                <img src="Group2.png" alt="" />
+              <div className="imgDiv flex justify-center items-center space-x-2 mr-4">
+              <img src="pin-light.png" alt="" />
+              <img src="3dots.png" alt="" />
               </div>
             </div>
 
-            <div className="border border-gray-200 p-2 rounded-lg mt-5 justify-between flex bg-[#FAFAFA] w-[95%] mx-auto">
+            <div className="border border-gray-200 p-1 rounded-lg mt-5 justify-between flex bg-[#FAFAFA] w-[95%] mx-auto">
               <div className="textDiv">
                 <h1>Meeting HR Department</h1>
                 <h1 className="text-sm">5 min ago</h1>
               </div>
-              <div className="imgDiv flex justify-center items-center gap-2">
-                <img src="Group2.png" alt="" />
-                <img src="Group2.png" alt="" />
+              <div className="imgDiv flex justify-center items-center space-x-2 mr-4">
+                <img src="pin-dark.png" alt="" />
+                <img src="3dots.png" alt="" />
               </div>
             </div>
 
@@ -110,24 +122,25 @@ function Main() {
                 </h1>
                 <h1 className="text-sm font-thin">Yesterday, 09:15 AM</h1>
               </div>
-              <div className="imgDiv flex justify-center items-center gap-2">
-                <img src="Group2.png" alt="" />
-                <img src="Group2.png" alt="" />
+              <div className="imgDiv flex justify-center items-center space-x-2 mr-4">
+              <img src="pin-dark.png" alt="" />
+              <img src="3dots.png" alt="" />
               </div>
             </div>
 
             <div className="border border-t-gray-200 border-b-0 border-l-0 border-r-0  flex justify-center w-full">
-              <p className="text-red-500 p-4"> See All Anouncement</p>
+              <p className="text-red-500 p-4 font-bold"> See All Anouncement</p>
             </div>
           </div>
         </div>
 
-        <div className=" flex sm:flex-col md:flex-row lg:flex-col xl:w-[40%] mt-6 sm:space-x-3">
-          <div className="blueContainer bg-[#161E54] text-white rounded-2xl ml-2 shadow-md p-3 ">
-            <h1 className="text-white bg-[#1B204A] p-2 mt-3">
+        {/*second div */}
+        <div className="lastContainer mb-5 md:p-5 lg:p-0 flex sm:flex-col md:flex-row lg:flex-col md:space-x-2 md:mr-10 sm:w-full xl:w-[35%] mr-auto mt-8 lg:space-x-3">
+          <div className="blueContainer bg-[#161E54] text-white rounded-2xl sm:mt-5 lg:mt-10 lg:ml-3 xl:mt-8 p-3 shadow-md">
+            <h1 className="text-white bg-[#1B204A] rounded-2xl text-xl p-4">
               Recently Activity
             </h1>
-            <p className="text-[10px] p-2 mt-3">10:40 AM, Fri Sept 2021</p>
+            <p className="text-[14px] p-2 mt-3">10:40 AM, Fri Sept 2021</p>
             <p className="p-2 text-xl font-bold tracking-wide">
               You Posted a New Job
             </p>
@@ -135,32 +148,33 @@ function Main() {
               Kindly check the requirements and terms of work and<br></br> make
               sure everything is right.
             </p>
-            <div className="bottom flex justify-between items-center mt-10">
+            <div className="bottom flex sm:py-3 sm:space-y-3 md:py-0 md:space-y-0 sm:mt-3 md:mt-10 sm:flex-col lg:flex-row lg:justify-between lg:items-center">
               <p className="p-2 ">Today you made 12 activities</p>
-              <button className="bg-red-500 rounded-md p-3 mr-4">
+              <button className="bg-red-500 rounded-md sm:p-2 lg:p-3 sm:w-1/3 md:w-2/3 lg:w-1/3  lg:mr-8">
                 See all activity
               </button>
             </div>
           </div>
 
-          <div className="bottomContainer lg:mt-10 border sm:w-full border-gray-200 rounded-2xl shadow-lg relative overflow-hidden">
-            <div className="top flex justify-between items-center p-3">
+          <div className="bottom sm:mt-5 lg:mt-6 border sm:w-full border-gray-200 rounded-2xl shadow-lg relative overflow-hidden">
+            <div className="top flex justify-between items-center sm:p-5 lg:p-3">
               <p className="font-bold text-3xl text-[#161E54] p-2 mt-4">
                 Upcoming Schedule
               </p>
-              <button className="text-sm border border-gray-200 p-2">
-                See all activity
-              </button>
+              <button className="text-sm border border-gray-200 p-2 flex justify-center items-center">
+                Today, 13 Sept 2021
+                <img src="Frame 10.png" alt="" />
+               </button>
             </div>
 
-            <p className="mt-4 font-thin p-2">Priority</p>
+            <p className="font-thin p-2">Priority</p>
             <div className="border border-gray-200 p-2 rounded-lg mt-2 justify-between flex bg-[#FAFAFA] w-[95%] mx-auto">
               <div className="textDiv">
                 <h1 className="">Review Candidate Applications</h1>
                 <h1 className="text-sm">5 min ago</h1>
               </div>
               <div className="imgDiv flex justify-center items-center p-2">
-                <img src="Group2.png" alt="" />
+                <img src="3dots.png" alt="" />
               </div>
             </div>
 
@@ -171,27 +185,24 @@ function Main() {
                 <h1 className="text-sm ">5 min ago</h1>
               </div>
               <div className="imgDiv flex justify-center items-center p-2">
-                <img src="Group2.png" alt="" />
+                <img src="3dots.png" alt="" />
               </div>
             </div>
 
-            <div className="border border-gray-200 p-3 rounded-lg mt-3 justify-between flex bg-[#FAFAFA] w-[95%] mx-auto">
+            <div className="flex md:hidden lg:flex border border-gray-200 p-3 rounded-lg mt-3 justify-between bg-[#FAFAFA] w-[95%] mx-auto">
               <div className="textDiv">
-                <p className="">
-                  Short Meeting with Product Designer <br />
-                  from IT Department
+                <p>
+                  Short Meeting with Product Designer <br /> from IT Department
                 </p>
-                <h1 className="text-sm font-thin ">5 min ago</h1>
+                <h1 className="text-sm font-thin">5 min ago</h1>
               </div>
               <div className="imgDiv flex justify-center items-center gap-2">
-                <img src="Group2.png" alt="" />
+                <img src="3dots.png" alt="" />
               </div>
             </div>
 
-            {/* New Partially Visible Div */}
-
             <div className="border border-t-gray-200 border-b-0 border-l-0 border-r-0 flex justify-center w-full">
-              <p className="text-red-500 p-4"> See All Anouncement</p>
+              <p className="text-red-500 p-4 font-bold"> Create a new Schedule</p>
             </div>
           </div>
         </div>
